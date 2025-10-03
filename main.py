f5f3620e511ccd3b859889fdaf9e51a69e674b2a
yahiarezk797 <yahiarezk797@gmail.com>
2025-10-03 14:01:49 +0300

    sitting the routes

--- main.py
+++ main.py
@@ -1,6 +1,31 @@
-def main():
-    print("Hello from nasa!")
+import os
+
+from flask import Flask, flash, jsonify, redirect, render_template, request, session
+
+# Configure application
+app = Flask(__name__)
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 
-if __name__ == "__main__":
-    main()
+
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+@app.route("/")
+def index():
+    pass
+
+
+
+
+
+@app.route("/results")
+def results():
+    pass
